4317c88f4eb6e476dd549f2784c74e88acde69e1
abhishek <abhisheksahu5395@gmail.com>
2025-08-30 18:48:42 +0530

    Initial commit: Spring Boot URL Shortening Service

---
+++ src/main/java/com/example/av/url_shortening/controller/UrlShorteningController.java
@@ -0,0 +1,66 @@
+package com.example.av.url_shortening.controller;
+
+import com.example.av.url_shortening.model.Url;
+import com.example.av.url_shortening.model.UrlDto;
+import com.example.av.url_shortening.model.UrlErrorResponseDto;
+import com.example.av.url_shortening.model.UrlResponseDto;
+import com.example.av.url_shortening.service.UrlService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@RestController
+public class UrlShorteningController {
+
+    @Autowired
+    private UrlService urlService;
+
+    @PostMapping("/generate")
+    public ResponseEntity<?> generateShortLink(@RequestBody UrlDto urlDto){
+        Url urlToRet = urlService.generateShortLink(urlDto);
+        if(urlToRet != null){
+            UrlResponseDto urlResponseDto = new UrlResponseDto();
+            urlResponseDto.setOriginalUrl(urlToRet.getOriginalUrl());
+            urlResponseDto.setExpirationDate(urlToRet.getExpirationDate());
+            urlResponseDto.setShortLink(urlToRet.getShortLink());
+            return new ResponseEntity<UrlResponseDto>(urlResponseDto,HttpStatus.OK);
+        }
+
+        UrlErrorResponseDto urlErrorResponseDto = new UrlErrorResponseDto();
+        urlErrorResponseDto.setStatus("404");
+        urlErrorResponseDto.setError("there was an error processing your request, please try again.");
+        return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/{shortLink}")
+    public ResponseEntity<?> redirectToOriginalUrl(@PathVariable String shortLink, HttpServletResponse response) throws IOException {
+        if(StringUtils.isNotEmpty(shortLink)){
+            UrlErrorResponseDto urlErrorResponseDto = new UrlErrorResponseDto();
+            urlErrorResponseDto.setError("Invalid Url");
+            urlErrorResponseDto.setStatus("400");
+            return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto, HttpStatus.OK);
+        }
+        Url urlToRet = urlService.getEncodedUrl(shortLink);
+        if(urlToRet == null){
+            UrlErrorResponseDto urlErrorResponseDto = new UrlErrorResponseDto();
+            urlErrorResponseDto.setError("Url doesn't exist or it might have expired");
+            urlErrorResponseDto.setStatus("400");
+            return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto, HttpStatus.OK);
+        }
+        if(urlToRet.getExpirationDate().isBefore(LocalDateTime.now())){
+            urlService.deleteShortLink(urlToRet);
+            UrlErrorResponseDto urlErrorResponseDto = new UrlErrorResponseDto();
+            urlErrorResponseDto.setError("Url expired. Please try generating a fresh one");
+            urlErrorResponseDto.setStatus("200");
+            return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto, HttpStatus.OK);
+        }
+        response.sendRedirect(urlToRet.getOriginalUrl());
+        return null;
+    }
+}
---
+++ src/main/java/com/example/av/url_shortening/service/UrlService.java
@@ -0,0 +1,14 @@
+package com.example.av.url_shortening.service;
+
+import com.example.av.url_shortening.model.Url;
+import com.example.av.url_shortening.model.UrlDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UrlService {
+
+    public Url generateShortLink(UrlDto urlDto);
+    public Url persistShortLink(Url url);
+    public Url getEncodedUrl(String url);
+    public void deleteShortLink(Url url);
+}
